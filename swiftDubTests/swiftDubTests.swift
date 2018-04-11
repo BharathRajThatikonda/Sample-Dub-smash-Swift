7112a2f5d2c7224cef61caccacd966ab3144da8f
Bharath Thatikonda <bthatikonda@gemini-us.com>
2018-04-11 15:38:42 +0530

    Initial Commit

---
+++ swiftDubTests/swiftDubTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftDubTests.swift
+//  swiftDubTests
+//
+//  Created by Bharath thaikonda on 11/04/18.
+//  Copyright © 2018 GCS. All rights reserved.
+//
+
+import XCTest
+@testable import swiftDub
+
+class swiftDubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
